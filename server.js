36d08b9729c49ae4fe7ec3f79dcbf0d43b96e89d
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-04 17:25:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article one is here');
+  res.send('Article one is here');
 });
 
 app.get('/article-two', function (req, res) {
