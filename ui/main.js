3cb905fde42ce23666f235a7042ef7638aa609e1
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-08 09:09:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,14 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 var img = document.getElementById('madi');
+function moveRight() {
+    var marginLeft = 0;
+    var moveLeft = moveLeft + 100;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
     
-    var interval = setInterval("moveRight,100");
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
     
     
