ce376213c37e13d82411bf410255170df99de142
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-08 09:12:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var img = document.getElementById('madi');
 
 function moveRight() {
    
-    var moveLeft = moveLeft + 100;
+    var marginLeft = marginLeft + 100;
     img.style.marginLeft = marginLeft + 'px';
 }
 
