ba2822dff1c264bd248206070eb2eded4ee54196
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-08 08:57:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Loaded!');
 // i am going to go to add some change
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+    
+    img.style.marginLeft = '100px';
+    
+    
+};
