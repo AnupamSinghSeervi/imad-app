cbe7913ebb5010470c4abe7e3a7169d9199fcd47
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-16 19:39:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/submit-name',function(req,res){
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     
    res.send(createTemplate(articles[articleName]));
