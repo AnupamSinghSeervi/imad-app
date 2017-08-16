db9ca812024c296ae80dfca0aede5108c3a7fd7b
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-16 17:44:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    user: 'anupamwants',
+    database : 'anupamwants',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -127,10 +134,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var Pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     //make a select request 
     //get result as a response
-    
+    Pool.query('SELECT * FROM test',function(err,result) {
+        if(err)  {
+        res.status(500).send(err.toString());
+    }
+    else {
+        res.send(JSON.stringify(result));
+    }
+    });
 });
 
 
