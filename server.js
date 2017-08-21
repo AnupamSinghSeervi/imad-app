1ef9221c42377fe3fe536bf69debdcb83e2df07c
AnupamSinghSeervi <anupamwants@gmail.com>
2017-08-21 20:24:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var config = {
     user: 'anupamwants',
     database : 'anupamwants',
     host : 'db.imad.hasura-app.io',
     port : '5432',
     password : process.env.DB_PASSWORD
-}
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -143,10 +144,28 @@ function hash(input,salt){
                    .digest('hex');
     return hashed.toString('hex');
 }
+
 app.get('/hash/:input',function(req,res)
 {
    var hashedString = hash(req.params.input,'this is and up with just an random string');
    res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res) {
+   
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+        if(err)  {
+        res.status(500).send(err.toString());
+    }
+    else {
+        res.send('user successfully created'+username);
+    }
+    });
 });
 
 var Pool = new Pool(config);
